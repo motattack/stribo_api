7478a2d336cbd479ce1b6128f5e7c9c1839a8d03
motattack <malrixinc@gmail.com>
2022-12-17 21:58:27 +1000

    added post method

--- authentication/views.py
+++ authentication/views.py
@@ -10,3 +10,10 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
