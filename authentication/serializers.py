90f12cb6fa8154348fc864ef4cd9432962307435
motattack <malrixinc@gmail.com>
2022-12-18 22:23:59 +1000

    fix hashing

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -11,7 +11,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('auth_token', 'name', 'email', 'level', 'exp', 'needExpToNextLevel', 'last_login', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
 
     def get_auth_token(self, obj):
         return Token.objects.get(user=obj).key
@@ -23,6 +22,17 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.name = validated_data.get('name', instance.name)
+        instance.level = validated_data.get('level', instance.level)
+        instance.exp = validated_data.get('exp', instance.exp)
+        instance.needExpToNextLevel = validated_data.get('needExpToNextLevel', instance.needExpToNextLevel)
+        instance.last_login = validated_data.get('last_login', instance.last_login)
+        instance.set_password(validated_data.get('password', instance.password))
+        instance.save()
+        return instance
 
 
 class UserRankingSerializer(serializers.ModelSerializer):
--- trainings/admin.py
+++ trainings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from trainings.models import Training
+
 # Register your models here.
+admin.site.register(Training)
